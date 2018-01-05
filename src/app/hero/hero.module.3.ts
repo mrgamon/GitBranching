8d4a62bca66df39de496fc69dd92bf72ee54e65d
Miles Pallarco <bluewaras3@gmail.com>
2018-01-06 03:07:19 +0800

    Added new module for hero

---
+++ src/app/hero/hero.module.3.ts
@@ -0,0 +1,19 @@
+import { NgModule }            from '@angular/core';
+import { CommonModule }        from '@angular/common';
+import { FormsModule }         from '@angular/forms';
+
+import { HeroComponent }       from './hero.component.3';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroListComponent }   from './hero-list.component';
+import { HeroRoutingModule }   from './hero-routing.module.3';
+
+import { HighlightDirective }  from './highlight.directive';
+
+@NgModule({
+  imports: [ CommonModule, FormsModule, HeroRoutingModule ],
+  declarations: [
+    HeroComponent, HeroDetailComponent, HeroListComponent,
+    HighlightDirective
+  ]
+})
+export class HeroModule { }
